82f881f0c8c9016e5042f52aeb6927edffc5c382
MatheusGorga <matheusgobar@gmail.com>
2023-06-09 16:33:21 -0300

    up

--- src/components/Produto/index.js
+++ src/components/Produto/index.js
@@ -1,40 +1,36 @@
-import { Container } from './styles';
-import { memo } from 'react';
-import { IconButton } from '@material-ui/core';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
+import { Container } from "./styles";
+import { memo } from "react";
+import { IconButton } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
+import { useCarrinhoContext } from "commons/context/Carrinho";
 
-
-function Produto({
-  nome,
-  foto,
-  id,
-  valor,
-  unidade
-}) {
+function Produto({ nome, foto, id, valor, unidade }) {
+  const { carrinho, adicionarProduto } = useCarrinhoContext() ;
+  const produtoNoCarrinho = carrinho.find(itemCarrinho => itemCarrinho.id === id)
+  
   return (
-      <Container>
-        <div>
-          <img
-            src={`/assets/${foto}.png`}
-            alt={`foto de ${nome}`}
-          />
-          <p>
-            {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
-          </p>
-        </div>
-        <div>
-          <IconButton
-            color="secondary"
-          >
-            <RemoveIcon />
-          </IconButton>
-          <IconButton>
-            <AddIcon />
-          </IconButton>
-        </div>
-      </Container>
-  )
+    <Container>
+      <div>
+        <img src={`/assets/${foto}.png`} alt={`foto de ${nome}`} />
+        <p>
+          {nome} - R$ {valor?.toFixed(2)} <span>Kg</span>
+        </p>
+      </div>
+      <div>
+        <IconButton color="secondary">
+          <RemoveIcon />
+        </IconButton>
+         
+          {produtoNoCarrinho?.quantidade || 0}
+         
+
+        <IconButton onClick={() => adicionarProduto({ nome, foto, id, valor })}>
+          <AddIcon />
+        </IconButton>
+      </div>
+    </Container>
+  );
 }
 
-export default memo(Produto)
+export default memo(Produto);
