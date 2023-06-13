a50c2c85697daa74b1c84dad4398038100a01c29
MatheusGorga <matheusgobar@gmail.com>
2023-06-13 16:09:52 -0300

    up

--- src/pages/Feira/index.js
+++ src/pages/Feira/index.js
@@ -8,7 +8,6 @@ import Produto from 'components/Produto';
 import NavBar from './NavBar';
 import { useContext } from 'react';
 import { UsuarioContext } from 'commons/context/Usuarios';
-import { useCarrinhoContext } from 'commons/context/Carrinho';
 
 
 function Feira() {
